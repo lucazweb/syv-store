bc840f6da13ed078cc30c9e68164615ff2061fca
Lucas <lucazwebmail@gmail.com>
2023-10-05 10:44:13 -0300

    feat: add InputSearch component
    - also add to home page

---
+++ src/presentation/components/input-search/index.tsx
@@ -0,0 +1,31 @@
+import React, { InputHTMLAttributes, useState } from 'react'
+import { StyledInput } from './styled'
+
+export interface InputSearchProps
+  extends InputHTMLAttributes<HTMLInputElement> {
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void
+}
+
+export const InputSearch = (props: InputSearchProps) => {
+  const [value, setValue] = useState('')
+
+  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const regex = /[^\w\s]/gi
+    if (regex.test(e.target.value)) {
+      setValue(e.target.value.replace(/[^\w\s]/gi, ''))
+    } else {
+      setValue(e.target.value)
+    }
+  }
+
+  return (
+    <StyledInput
+      data-testid="input-search-component"
+      {...props}
+      value={value}
+      onChange={(e) => {
+        handleOnChange(e)
+      }}
+    />
+  )
+}
--- src/presentation/pages/home.tsx
+++ src/presentation/pages/home.tsx
@@ -1,6 +1,23 @@
 import React from 'react'
+import { Grid, Row, Col } from 'react-flexbox-grid'
 import { Header } from '../components/header'
+import { InputSearch } from '../components/input-search'
 
 export const Home = () => {
-  return <Header />
+  return (
+    <>
+      <Header />
+      <Grid>
+        <Row style={{ marginTop: '32px' }}>
+          <Col md={10}>
+            <Row>
+              <Col md={10}>
+                <InputSearch placeholder="Pesquisar por nome ou número" />
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </Grid>
+    </>
+  )
 }
