04df0d19baee4d1bba3d89b2632ff15a41fae02f
Lucas <lucazwebmail@gmail.com>
2023-10-04 22:59:29 -0300

    style: add global styles

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
+import { GlobalStyle } from '@/presentation/styles/global'
 import App from './app'
 
 const rootElement = document.getElementById('root')
 const root = createRoot(rootElement)
 
-root.render(<App />)
+root.render(
+  <>
+    <GlobalStyle />
+    <App />
+  </>
+)
